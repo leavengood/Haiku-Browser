b9491ec3fb67122c9689aebb210f8e826a3f551c
Maxime Simon <maxime@macbook-de-maxime-simon.local>
2009-05-19 10:17:50 +0200

    A first design of a bookmarking library.

---
+++ libbookmark/Bookmark.h
@@ -0,0 +1,41 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+//  Copyright 2009 Haiku Inc. All rights reserved.
+//  Distributed under the terms of the MIT License.
+//
+//  Author:
+// 			Maxime Simon, simon.maxime (at) gmail.com
+// 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef BOOKMARK_H
+#define BOOKMARK_H
+
+
+#include <Bitmap.h>;
+#include <Directory.h>;
+#include <String.h>;
+
+class Bookmark {
+	public:
+		Bookmark(BString *title, BString *url, BDirectory *parentDirectory = NULL,
+		        BBitmap *favicon = NULL);
+		~Bookmark();
+
+		BString*	Title();
+		BString*	Url();
+		BDirectory*	ParentDirectory();
+		BBitmap*	Favicon();
+		void		setTitle(BString *title);
+		void		setUrl(BString *url);
+		void		setParentDirectory(BDirectory *parentDirectory);
+		void		setFavicon(BBitmap *favicon);
+
+	private:
+		BString		*fTitle;
+		BString		*fUrl;
+		BDirectory	*fParentDirectory;
+		BBitmap		*fFavicon;
+};
+
+#endif // BOOKMARK_H
---
+++ libbookmark/BookmarkManager.h
@@ -0,0 +1,32 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+//  Copyright 2009 Haiku Inc. All rights reserved.
+//  Distributed under the terms of the MIT License.
+//
+//  Author:
+// 			Maxime Simon, simon.maxime (at) gmail.com
+// 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef BOOKMARK_MANAGER_H
+#define BOOKMARK_MANAGER_H
+
+
+#include <List.h>
+
+#include "Bookmark.h"
+
+class BookmarkManager {
+	public:
+		BookmarkManager(BDirectory *directory = NULL);
+		~BookmarkManager();
+
+		void	AddBookmark(Bookmark *bookmark);
+		BList*	GetBookmarkList(BDirectory *directory, bool recursively = true);
+		void	ManageBookmarks();
+
+	private:
+		BDirectory	*fBookmarksDirectory
+};
+
+#endif // BOOKMARK_MANAGER_H
